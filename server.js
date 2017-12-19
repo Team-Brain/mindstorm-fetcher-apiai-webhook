9588a6c2ed1449dac3daf3475ec030c164c82e64
Marek Abaffy <abaffy.m@gmail.com>
2017-12-19 20:55:06 +0100

    Refactoring

--- server.js
+++ server.js
@@ -14,27 +14,22 @@ const is_staging = process.env.ENV === 'STAGING'
 app.use(bodyParser.json({ type: 'application/json' }))
 http.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
-// Ordered list of requests for Fetchy to perform
+// Ordered list of requests for the robot to perform
 var taskQueue = []
 
-// Triggered by a POST to /webhook 
 app.post('/api/v1/webhook', (request, response) => {
 
-    console.log('Request body: ' + JSON.stringify(request.body))
     let action = request.body.result.action
-    console.log('result action: ' + action)
+    console.log('Request action: ' + action)
     const parameters = request.body.result.parameters
-    console.log('')
 
-    // responseJSON is used to send back to dialogflow
-    // requestJSON is used to send the request to the robot
+    // Response to Dialogflow
     let responseJson = {}
-    let requestJson = {}
+    // Task data to the robot
+    let taskJson = {}
 
-    // A handler for each action defined in Dialogflow
     const actionHandlers = {
 
-        // An intent to send send a response to Dialogflow, and put tasks into the taskQueue
         'bring_object': () => {
             let color = parameters['color']
             let object = parameters['object']
@@ -42,17 +37,20 @@ app.post('/api/v1/webhook', (request, response) => {
                 responseJson.speech = `Bringing the ${color} ${object}.`
                 responseJson.displayText = `Bringing the ${color} ${object}.`
 
-                requestJson.action = action
-                requestJson.color = color
-                requestJson.object = object
-                requestJson.timestamp = new Date()
+                taskJson.action = action
+                taskJson.color = color
+                taskJson.object = object
+                taskJson.timestamp = new Date()
 
-                // unique id is created by stringifying the JSON object and hashing it
-                var requestStringed = JSON.stringify(requestJson)
+                // Generate unique id by hashing the object
+                var requestStringed = JSON.stringify(taskJson)
                 var hash_id = crypto.createHash('md5').update(requestStringed).digest('hex');
-                requestJson.id = hash_id
+                taskJson.id = hash_id
 
-                addToTaskQueue(requestJson)
+                console.log(`Add task to task queue: ${JSON.stringify(taskJson)}`)
+                taskQueue.push(taskJson)
+                console.log(`Items in the queue: ${JSON.stringify(taskQueue)}`)
+                
                 emitTask()
 
             } else {
@@ -64,61 +62,53 @@ app.post('/api/v1/webhook', (request, response) => {
             response.json(responseJson)
 
         },
-        // The default intent when an action has not been defined or understood in the user input
+
         'queue_contents': () => {
             var queueContentString = queueContents()
-            responseJson.speech = `${queueContentString}`
-            responseJson.displayText = `${queueContentString}`
+            responseJson.speech = queueContentString
+            responseJson.displayText = queueContentString
             response.json(responseJson)
         },
-        // The default intent when an action has not been defined or understood in the user input
-        'default': () => {
-            responseJson.speech = 'Sorry, I dont understand what you said. Please repeat the request'
-            responseJson.displayText = 'Sorry, I dont understand what you said. Please repeat the request'
-            response.json(responseJson)
-        },
-        // An intent to remove the first request in the request queue
-        'cancel_current': () => {
 
+        'cancel_current': () => {
             if (taskQueue[0] == null) {
                 console.log('No requests to abort')
                 responseJson.speech = 'There are no requests to abort'
                 responseJson.displayText = 'There are no requests to abort'
-                response.json(responseJson)
             } else {
                 responseJson.speech = `I am aborting the current request`
                 responseJson.displayText = `I am aborting the current request`
-                response.json(responseJson)
                 io.emit('abort')
             }
+            response.json(responseJson)
         },
-        // An intent to remove the all requests in the request queue
-        'cancel_all': () => {
 
+        'cancel_all': () => {
             if (taskQueue[0] == null) {
                 console.log('No requests to abort')
                 responseJson.speech = 'No requests to abort'
                 responseJson.displayText = 'No requests to abort'
-                response.json(responseJson)
-            }
-            else {
+            } else {
                 responseJson.speech = 'I am aborting all requests'
                 responseJson.displayText = 'I am aborting all requests'
-                response.json(responseJson)
 
                 taskQueue = []
-                console.log('removing all requests')
-                console.log('')
-
                 io.emit('abort')
             }
+            response.json(responseJson)            
+        },
+
+        'default': () => {
+            responseJson.speech = 'Sorry, I dont understand what you said. Please repeat the request'
+            responseJson.displayText = 'Sorry, I dont understand what you said. Please repeat the request'
+            response.json(responseJson)
         }
     }
 
     if (!actionHandlers[action]) {
         action = 'default'
     }
-    // Matches the action to a action handler
+
     actionHandlers[action]()
 
 })
@@ -136,20 +126,14 @@ function queueContents() {
     return "There are no requests in the queue"
 }
 
-function addToTaskQueue(request) {
-    console.log(`adding task to task queue: ${JSON.stringify(request)}`)
-    taskQueue.push(request)
-    console.log(`current items in taskQueue after add: ${JSON.stringify(taskQueue)}`)
-}
-
 function finishedTask(taskId) {
     if (taskQueue.length != 0 && taskId == taskQueue[0].id) {
         console.log(`Fetchy finished task: ${JSON.stringify(taskQueue[0])}`)
         taskQueue.shift()
-        console.log(`request removed, current items in taskQueue: ${JSON.stringify(taskQueue)}`)
-        console.log('')
+        console.log(`Items in the taskQueue: ${JSON.stringify(taskQueue)}`)
     } else {
-        console.log('wtf just happened')
+        console.log('Finished task ids do not match')
+        // Ignore
     }
     emitTask()
 }
